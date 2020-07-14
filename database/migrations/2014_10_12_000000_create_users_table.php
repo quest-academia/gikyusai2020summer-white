560a62302a3d944fd79827a0de33656ba5f26117
Sasaki1994m <62878118+Sasaki1994m@users.noreply.github.com>
2020-07-14 22:15:46 +0900

    create_users_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('icon')->nullable();
+            $table->integer('role')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
