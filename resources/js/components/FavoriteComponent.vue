6685d7a0c93d927ab5ca5b3af858856c54b557d8
SekiYukihiro <shiba920sof@yahoo.co.jp>
2020-07-17 20:40:14 +0900

    add:FavoriteComponent

---
+++ resources/js/components/FavoriteComponent.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<button v-if="flag" v-on:click="changeFavorite">いいね済</button>
+		<button v-else v-on:click="changeFavorite">いいね</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				flag: false
+			}
+		},
+		methods: {
+			changeFavorite () {
+				this.flag = !this.flag
+			}
+		}
+	}
+</script>
