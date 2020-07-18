2bf693f0c10f1580a1ddf60da50453bab0a19c22
hsg0830 <hsg0830@gmail.com>
2020-07-18 00:10:05 +0000

    create challenges table migration

---
+++ database/migrations/2020_07_17_235404_create_challenges_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('recipie_id');
+            $table->string('img');
+            $table->text('impression');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('challenges');
+    }
+}
