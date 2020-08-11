6f33edb49fb51d8c041abbda3ff90236d0601d88
hsg0830 <hsg0830@gmail.com>
2020-08-11 07:30:59 +0000

    ユーザー登録画面作成およびヘッダーなどの変更

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.challenges')
 
 @section('content')
 <div id="app">
+    <h1 style="text-align: center;">トップページ</h1>
+    <h2 style="text-align: center; color: red;">現在制作中です。</h2>
+
     <router-view></router-view>
 </div>
 @endsection
